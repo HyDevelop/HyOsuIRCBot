e7d130dc6cfce9497d391a952bf10abc548fbf88
Hykilpikonna <admin@moecraft.cc>
2018-04-21 19:03:09 -0400

    添加了两条测试指令

---
+++ src/cc/moecraft/irc/osubot/command/commands/CommandPing.java
@@ -0,0 +1,28 @@
+package cc.moecraft.irc.osubot.command.commands;
+
+import cc.moecraft.irc.osubot.command.Command;
+import org.pircbotx.Channel;
+import org.pircbotx.User;
+import org.pircbotx.hooks.types.GenericMessageEvent;
+
+import java.util.ArrayList;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/04/21 创建!
+ * Created by Hykilpikonna on 2018/04/21!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ */
+public class CommandPing extends Command
+{
+    public CommandPing()
+    {
+        super("ping", new ArrayList<>());
+    }
+
+    @Override
+    public void run(GenericMessageEvent event, User sender, Channel channel, String command, ArrayList<String> args)
+    {
+        event.respond("Pong!");
+    }
+}
