dd5f5a3fce885accba1ba0a5b18e8eda91d8d0a2
Hykilpikonna <admin@moecraft.cc>
2018-04-21 19:08:01 -0400

    忘记注册指令了 (

---
+++ src/cc/moecraft/irc/osubot/language/Messenger.java
@@ -0,0 +1,22 @@
+package cc.moecraft.irc.osubot.language;
+
+import org.pircbotx.hooks.types.GenericMessageEvent;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/04/21 创建!
+ * Created by Hykilpikonna on 2018/04/21!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ */
+public class Messenger
+{
+    public Messenger()
+    {
+
+    }
+
+    public void respond(GenericMessageEvent event, String text)
+    {
+
+    }
+}
