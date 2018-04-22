da4943683653aa8739275e4722ce8d959469348e
Hykilpikonna <admin@moecraft.cc>
2018-04-22 14:16:25 -0400

    [+] 重载指令

---
+++ src/main/java/cc/moecraft/irc/osubot/command/commands/management/CommandReload.java
@@ -0,0 +1,34 @@
+package cc.moecraft.irc.osubot.command.commands.management;
+
+import cc.moecraft.irc.osubot.Main;
+import cc.moecraft.irc.osubot.command.Command;
+import org.pircbotx.Channel;
+import org.pircbotx.User;
+import org.pircbotx.hooks.types.GenericMessageEvent;
+
+import java.util.ArrayList;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/04/22 创建!
+ * Created by Hykilpikonna on 2018/04/22!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ */
+public class CommandReload extends Command
+{
+    public CommandReload()
+    {
+        super("reload");
+    }
+
+    @Override
+    public void run(GenericMessageEvent event, User sender, Channel channel, String command, ArrayList<String> args)
+    {
+        Main.getMessenger().respond(event, "开始重载...");
+        long start = System.currentTimeMillis();
+
+        Main.getConfig().reload();
+
+        Main.getMessenger().respond(event, "重载完成! 耗时: " + (System.currentTimeMillis() - start) + "ms");
+    }
+}
