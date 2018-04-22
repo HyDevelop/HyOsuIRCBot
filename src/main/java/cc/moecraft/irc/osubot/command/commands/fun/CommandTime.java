c106db2eb6a712c129e4988d978fb8f564a12de1
Hykilpikonna <admin@moecraft.cc>
2018-04-22 12:44:59 -0400

    [+] 彩蛋指令

---
+++ src/main/java/cc/moecraft/irc/osubot/command/commands/fun/CommandTime.java
@@ -0,0 +1,30 @@
+package cc.moecraft.irc.osubot.command.commands.fun;
+
+import cc.moecraft.irc.osubot.Main;
+import cc.moecraft.irc.osubot.command.Command;
+import org.pircbotx.Channel;
+import org.pircbotx.User;
+import org.pircbotx.hooks.types.GenericMessageEvent;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/04/22 创建!
+ * Created by Hykilpikonna on 2018/04/22!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ */
+public class CommandTime extends Command
+{
+    public CommandTime()
+    {
+        super("time", new ArrayList<>(Arrays.asList("currenttime", "ct", "sj", "时间")));
+    }
+
+    @Override
+    public void run(GenericMessageEvent event, User sender, Channel channel, String command, ArrayList<String> args)
+    {
+        Main.getMessenger().respond(event, "当前时间: " + Main.getLogger().getCurrentTime());
+    }
+}
