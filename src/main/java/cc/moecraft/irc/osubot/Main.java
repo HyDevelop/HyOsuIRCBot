ff9e09292dd7ed2e5e39713da363c8590f1e39bb
Hykilpikonna <admin@moecraft.cc>
2018-04-22 19:28:58 -0400

    [U] 添加TODO

--- src/main/java/cc/moecraft/irc/osubot/Main.java
+++ src/main/java/cc/moecraft/irc/osubot/Main.java
@@ -78,7 +78,7 @@ public class Main {
     public static final String VERSION = "0.0.1";
 
     // 配置/语言文件路径
-    public static final String PATH = "src" + File.separator + "main" + File.separator + "resources";
+    public static final String PATH = "src" + File.separator + "main" + File.separator + "resources"; //TODO: 这里分两个版本, 测试放现在这个路径, 发布的话放"./conf/"路径
 
     // 配置文件 ( 用来存用于IRC登陆的账号密码 )
     private static BotConfig config;
