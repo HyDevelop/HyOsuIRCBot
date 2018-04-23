f3c6cec247939ed379ce914ac9e37e7e6f11ade9
Hykilpikonna <admin@moecraft.cc>
2018-04-23 17:48:39 -0400

    [+] HTTP下载工具类

---
+++ src/main/java/cc/moecraft/irc/osubot/utils/DownloadUtils.java
@@ -0,0 +1,129 @@
+package cc.moecraft.irc.osubot.utils;
+
+import cc.moecraft.irc.osubot.Main;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/04/23 创建!
+ * Created by Hykilpikonna on 2018/04/23!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ */
+public class DownloadUtils
+{
+    /**
+     * 下载文件
+     *
+     * @param url 下载地址
+     * @return 下载到的内容
+     */
+    public static byte[] downloadFile(URL url)
+    {
+        try
+        {
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setConnectTimeout(Main.getConfig().getInt("BotProperties.Downloading.Timeout"));
+            httpURLConnection.setReadTimeout(Main.getConfig().getInt("BotProperties.Downloading.Timeout"));
+            httpURLConnection.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+
+            InputStream inputStream = httpURLConnection.getInputStream();
+
+            byte[] data = readInputStream(inputStream);
+            inputStream.close();
+
+            return data;
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 读取InputStream
+     * 归功于 @V乐乐
+     * Credit to @Vlvxingze
+     *
+     * @param inputStream InputStream
+     * @return 读取为字节数组
+     * @throws IOException 抛出异常
+     */
+    public static byte[] readInputStream(InputStream inputStream) throws IOException
+    {
+        byte[] buffer = new byte[1024];
+        int len;
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        while ((len = inputStream.read(buffer)) != -1)
+        {
+            outputStream.write(buffer, 0, len);
+        }
+        outputStream.close();
+
+        return outputStream.toByteArray();
+    }
+
+    /**
+     * 判断当前版本和最新版本的关系
+     *
+     * 如果当前版本小于最新版本, 返回1,
+     * 如果当前版本大于最新版本, 返回-1,
+     * 如果当前版本等于最新版本, 返回0
+     *
+     * 注意: 所有非数字字符都会被忽略
+     *
+     * 例子: 0.1.6.9 和 0.1.6.9 返回0
+     * 例子: 0.1.6.9 和 0.1.7.0 返回1
+     * 例子: 0.1.69  和 0.17.0  返回1
+     * 例子: 0.16.9  和 0.1.70  返回-1
+     *
+     * @param currentVersion 当前版本
+     * @param latestVersion 最新版本
+     * @return 对比值
+     */
+    public static int versionComparison(String currentVersion, String latestVersion)
+    {
+        if (currentVersion == null || latestVersion == null) return 0;
+        String[] currentVersionAfterSplit = removeInNumeric(currentVersion).split("\\.");
+        String[] latestVersionAfterSplit = removeInNumeric(latestVersion).split("\\.");
+
+        int currentLength = currentVersionAfterSplit.length;
+        int latestLength = latestVersionAfterSplit.length;
+
+        for (int i = 0; i < Math.max(currentLength, latestLength); i++)
+        {
+            int currentVersionAtI = i < currentLength ? Integer.parseInt(currentVersionAfterSplit[i]) : 0;
+            int latestVersionAtI = i < latestLength ? Integer.parseInt(latestVersionAfterSplit[i]) : 0;
+
+            if (currentVersionAtI < latestVersionAtI) return 1;
+            if (currentVersionAtI > latestVersionAtI) return -1;
+        }
+        return 0;
+    }
+
+    /**
+     * 去掉字符串里除了数字/小数点外的字符 // TODO: 需要优化
+     *
+     * @param string 源
+     * @return 处理后
+     */
+    public static String removeInNumeric(String string)
+    {
+        if (string == null || string.equals("")) return "";
+
+        StringBuilder output = new StringBuilder();
+        for (Character aChar : string.toCharArray())
+        {
+            if (Character.isDigit(aChar)) output.append(aChar);
+            if (aChar == '.') output.append('.');
+        }
+        return output.toString();
+    }
+}
