57988ff2943434fcb48e680234e9a9d7216dd011
Hykilpikonna <admin@moecraft.cc>
2018-04-24 18:51:08 -0400

    [T] 反射测试

---
+++ src/test/java/com/dullwolf/ReflectTest.java
@@ -0,0 +1,35 @@
+package com.dullwolf;
+
+import cc.moecraft.irc.osubot.DebugLogger;
+import cc.moecraft.irc.osubot.osu.data.UserData;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/04/24 创建!
+ * Created by Hykilpikonna on 2018/04/24!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ */
+public class ReflectTest
+{
+    private static DebugLogger logger = new DebugLogger("Test", true);
+
+    public static void main(String[] args)
+    {
+        UserData userData = new UserData();
+
+        Field[] fields = UserData.class.getDeclaredFields();
+
+        StringBuilder fieldNames = new StringBuilder("[");
+
+        for (Field field : fields)
+        {
+            fieldNames.append(field.getName()).append(", ");
+        }
+
+        logger.debug("Fields:" + fieldNames.append("]").toString());
+        logger.debug("- length = " + fields.length);
+    }
+}
