db837c66e174c81643ba1d65a9d0172c339ee720
Hykilpikonna <admin@moecraft.cc>
2018-04-24 21:06:39 -0400

    [+] 反射工具类

---
+++ src/main/java/cc/moecraft/irc/osubot/utils/ReflectUtils.java
@@ -0,0 +1,26 @@
+package cc.moecraft.irc.osubot.utils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/04/24 创建!
+ * Created by Hykilpikonna on 2018/04/24!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ */
+public class ReflectUtils
+{
+    /**
+     * 反射获取变量值
+     * @param field Field
+     * @param object 变量
+     * @return 值
+     */
+    public static Object getValue(Field field, Object object) throws IllegalAccessException
+    {
+        field.setAccessible(true);
+        return field.get(object);
+    }
+}
