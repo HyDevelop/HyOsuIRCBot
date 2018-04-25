4372f491f0b67385a832db376e17a4c33e733c98
Hykilpikonna <admin@moecraft.cc>
2018-04-24 20:52:48 -0400

    [F] 运行时读取Tag

--- src/main/java/cc/moecraft/irc/osubot/osu/parameters/tags/HttpParameter.java
+++ src/main/java/cc/moecraft/irc/osubot/osu/parameters/tags/HttpParameter.java
@@ -1,11 +1,15 @@
 package cc.moecraft.irc.osubot.osu.parameters.tags;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * 此类由 Hykilpikonna 在 2018/04/24 创建!
  * Created by Hykilpikonna on 2018/04/24!
  * Github: https://github.com/hykilpikonna
  * Meow!
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface HttpParameter
 {
     boolean required();
