05b8c01e40fe6af9a12486b809407b5812dbae72
Hykilpikonna <admin@moecraft.cc>
2018-04-24 20:46:08 -0400

    [+] Http参数标注Tag

--- src/main/java/cc/moecraft/irc/osubot/osu/parameters/ParametersBase.java
+++ src/main/java/cc/moecraft/irc/osubot/osu/parameters/ParametersBase.java
@@ -10,9 +10,9 @@ import lombok.Data;
  * Meow!
  */
 @Data
-public abstract class ParameterBase
+public abstract class ParametersBase
 {
-    public abstract String getSubURL();
+    public abstract String subURL();
 
-    public abstract DataBase getDataStorage();
+    public abstract DataBase dataStorageObject();
 }
