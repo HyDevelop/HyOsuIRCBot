65d0722fa396d16044c496957554b6613ab145d7
Hykilpikonna <admin@moecraft.cc>
2018-04-24 21:12:08 -0400

    [T] 测试反射工具类

--- src/test/java/com/dullwolf/ReflectTest.java
+++ src/test/java/com/dullwolf/ReflectTest.java
@@ -2,6 +2,7 @@ package com.dullwolf;
 
 import cc.moecraft.irc.osubot.DebugLogger;
 import cc.moecraft.irc.osubot.osu.data.UserData;
+import cc.moecraft.irc.osubot.utils.ReflectUtils;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
@@ -16,20 +17,23 @@ public class ReflectTest
 {
     private static DebugLogger logger = new DebugLogger("Test", true);
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IllegalAccessException
     {
-        UserData userData = new UserData();
+        UserData userData = new UserData(1,"test",1,1,1,1,1,1,1,1.5d,1.7d,1.8d,1,1,1,1,1,"cn",1);
 
         Field[] fields = UserData.class.getDeclaredFields();
 
         StringBuilder fieldNames = new StringBuilder("[");
+        StringBuilder fieldDatas = new StringBuilder("[");
 
         for (Field field : fields)
         {
             fieldNames.append(field.getName()).append(", ");
+
+            fieldDatas.append(ReflectUtils.getValue(field, userData)).append(", ");
         }
 
-        logger.debug("Fields:" + fieldNames.append("]").toString());
-        logger.debug("- length = " + fields.length);
+        logger.debug("Field Names: " + fieldNames.append("]").toString());
+        logger.debug("Field Datas: " + fieldDatas.append("]").toString());
     }
 }
