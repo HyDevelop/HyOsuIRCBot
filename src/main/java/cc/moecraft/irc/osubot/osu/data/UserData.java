b8c101994e8581fddd6c3ce780b92cb4c0299146
Hykilpikonna <admin@moecraft.cc>
2018-04-25 15:30:36 -0400

    [U] UserData变量改成long类型

--- src/main/java/cc/moecraft/irc/osubot/osu/data/UserData.java
+++ src/main/java/cc/moecraft/irc/osubot/osu/data/UserData.java
@@ -2,8 +2,6 @@ package cc.moecraft.irc.osubot.osu.data;
 
 import lombok.*;
 
-import java.util.ArrayList;
-
 /**
  * 此类由 Hykilpikonna 在 2018/04/24 创建!
  * Created by Hykilpikonna on 2018/04/24!
@@ -14,34 +12,39 @@ import java.util.ArrayList;
 @Data @Builder @NoArgsConstructor @AllArgsConstructor
 public class UserData extends DataBase
 {
-    private int user_id;
+    private long user_id;
     private String username;
-    private int count300;
-    private int count100;
-    private int count50;
-    private int play_count;
-    private int ranked_score;
-    private int total_score;
-    private int pp_rank;
+    private long count300;
+    private long count100;
+    private long count50;
+    private long play_count;
+    private long ranked_score;
+    private long total_score;
+    private long pp_rank;
     private double level;
     private double pp_raw;
     private double accuracy;
-    private int count_rank_ss;
-    private int count_rank_ssh;
-    private int count_rank_s;
-    private int count_rank_sh;
-    private int count_rank_a;
+    private long count_rank_ss;
+    private long count_rank_ssh;
+    private long count_rank_s;
+    private long count_rank_sh;
+    private long count_rank_a;
     private String country;
-    private int pp_country_rank;
+    private long pp_country_rank;
+
+    /*
+    两层的反射不知道怎么做
+
     private ArrayList<UserEvent> events;
+
 
     @Data
     public class UserEvent
     {
         private String display_html;
-        private int beatmap_id;
-        private int beatmapset_id;
+        private long beatmap_id;
+        private long beatmapset_id;
         private String date;
-        private int epicfactor;
-    }
+        private long epicfactor;
+    }*/
 }
