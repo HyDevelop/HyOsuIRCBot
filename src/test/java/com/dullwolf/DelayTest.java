5f1a9eab84f3be241aed40552e35276b34ba4bd6
Hykilpikonna <admin@moecraft.cc>
2018-04-26 17:15:20 -0400

    [T] 测试获取Key用时

---
+++ src/test/java/com/dullwolf/DelayTest.java
@@ -0,0 +1,24 @@
+package com.dullwolf;
+
+import cc.moecraft.irc.osubot.DebugLogger;
+import cc.moecraft.irc.osubot.utils.PropertiesUtil;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/04/26 创建!
+ * Created by Hykilpikonna on 2018/04/26!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ */
+public class DelayTest
+{
+    private static DebugLogger logger = new DebugLogger("DelayTest", true);
+
+    public static void main(String[] args)
+    {
+        long startingTime = System.currentTimeMillis();
+
+        logger.log(PropertiesUtil.readKey("osu_key"));
+
+        logger.log("获取Key完成, 用时: " + (System.currentTimeMillis() - startingTime));
+    }
+}
