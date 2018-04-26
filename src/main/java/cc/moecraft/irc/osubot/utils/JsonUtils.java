61f2b128a4c9b7fe9a5ffdcf007638fa85529fea
Hykilpikonna <admin@moecraft.cc>
2018-04-26 16:10:32 -0400

    [U] JSON工具类改用GSON库

--- src/main/java/cc/moecraft/irc/osubot/utils/JsonUtils.java
+++ src/main/java/cc/moecraft/irc/osubot/utils/JsonUtils.java
@@ -1,71 +1,80 @@
 package cc.moecraft.irc.osubot.utils;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONException;
-import com.alibaba.fastjson.JSONObject;
-import com.jfinal.kit.StrKit;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
-import java.util.*;
-
-import static org.codehaus.groovy.runtime.DefaultGroovyMethods.toList;
-
+/**
+ * 此类由 Hykilpikonna 在 2018/58/26 创建!
+ * Created by Hykilpikonna on 2018/58/26!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ */
 public class JsonUtils {
 
     /**
-     * 解析json字符串 转换指定的对象
+     * 把JSON字符串转换为Java对象
+     *
      * @param json json字符串
-     * @param clazz 对象类型
+     * @param targetClass 对象类
      * @param <T> 泛型
-     * @return
+     * @return Java对象
      */
-    public static <T> T getObjectByJson(String json, Class<T> clazz) {
-        return JSON.parseObject(json, clazz);
+    public static <T> T getObjectByJson(String json, Class<T> targetClass)
+    {
+        return new Gson().fromJson(json, targetClass);
     }
 
     /**
-     * getObjectByJson的封装, 把JSON字符串转换成JSONObject
+     * 把JSON字符串转换成JsonElement对象
+     *
      * @param json JSON字符串
      * @return 转换后的JSONObject
      */
-    public static JSONObject getJsonObjectByJsonString(String json) {
-        return JSON.parseObject(json);
-    }
-
-    /**
-     * getJsonObjectByJsonString和getJsonString的封装, 把对象直接转换为JSONObject
-     * @param obj 目标对象
-     * @return 转换后的JSONObject
-     */
-    public static JSONObject getJsonObject(Object obj) {
-        return getJsonObjectByJsonString(getJsonString(obj));
-    }
-
-    /**
-     * 解析json字符串 转换指定的列表对象
-     * @param json json字符串
-     * @param clazz 列表对象
-     * @param <T> 泛型
-     * @return
-     */
-    public static <T> List<T> getArrayByJson(String json, Class<T> clazz){
-        return JSON.parseArray(json, clazz);
+    public static JsonElement parseJsonElement(String json)
+    {
+        return new JsonParser().parse(json);
     }
 
     /**
      * 解析对象转换为json字符串
+     *
      * @param obj 对象
      * @return json字符串
      */
-    public static String getJsonString(Object obj){
-        return JSONObject.toJSONString(obj);
+    public static String toJsonString(Object obj)
+    {
+        return new Gson().toJson(obj);
+    }
+
+    /**
+     * 把对象直接转换为JSONObject
+     *
+     * @param obj 目标对象
+     * @return 转换后的JSONObject
+     */
+    public static JsonElement toJsonElement(Object obj)
+    {
+        return parseJsonElement(toJsonString(obj));
+    }
+    /*
+    /**
+     * 解析json字符串 转换指定的列表对象
+     * @param json json字符串
+     * @param targetClass 列表对象
+     * @param <T> 泛型
+     * @return
+     *//*
+    public static <T> List<T> getArrayByJson(String json, Class<T> targetClass)
+    {
+        return .parseArray(json, targetClass);
     }
 
     /**
      * json字符串中下划线转为驼峰
      * @param json json字符串
      * @return 驼峰对象
-     */
+     *//*
     public static Object convert(String json) {
         Object obj = JSON.parse(json);
         convert(obj);
@@ -89,5 +98,5 @@ public class JsonUtils {
                 convert(value);
             }
         }
-    }
+    }*/
 }
