2491c52f0b3e707d637d0a7525d2fa63045568b7
Hykilpikonna <admin@moecraft.cc>
2018-04-27 22:04:00 -0400

    [U] 更新版本号

--- src/main/java/cc/moecraft/irc/osubot/Main.java
+++ src/main/java/cc/moecraft/irc/osubot/Main.java
@@ -30,7 +30,7 @@ import java.util.ArrayList;
  */
 public class Main {
     // 版本 ( 不懂怎样配置Github版本...
-    public static final String VERSION = "0.0.2";
+    public static final String VERSION = "0.0.3";
 
     // 配置/语言文件路径
     public static final String PATH = "src" + File.separator + "main" + File.separator + "resources"; //TODO: 这里分两个版本, 测试放现在这个路径, 发布的话放"./conf/"路径
