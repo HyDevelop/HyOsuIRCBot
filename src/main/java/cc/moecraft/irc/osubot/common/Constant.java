c28a48a0a600bf3a956dce65bbd4f0f6c0b9742a
dullwolf <1091569752@qq.com>
2018-04-30 00:08:17 +0800

    定时任务 --> 爬OSU官方所有用户信息（已完成）

---
+++ src/main/java/cc/moecraft/irc/osubot/common/Constant.java
@@ -0,0 +1,22 @@
+package cc.moecraft.irc.osubot.common;
+
+
+/**
+ * 常量类
+ *
+ * @author dullwolf
+ */
+public class Constant {
+
+
+    /**
+     * 操作日志队列
+     */
+    public static final String QUEUE_LOGS = "queueLog";
+
+    /**
+     * OSU用户最大maxId标记
+     */
+    public static final String USER_MAX_ID = "userMaxId";
+
+}
