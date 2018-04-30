3c6180aca8cc39bc11f76884f2146c87e4e42084
dullwolf <1091569752@qq.com>
2018-04-30 14:37:28 +0800

    提交测试类，测试异步执行时是否与网速有关

--- src/test/java/com/dullwolf/CallableAndFuture.java
+++ src/test/java/com/dullwolf/CallableAndFuture.java
@@ -3,9 +3,18 @@ package com.dullwolf;
 
 import cc.moecraft.irc.osubot.common.Constant;
 import cc.moecraft.irc.osubot.factory.DAOFactory;
+import cc.moecraft.irc.osubot.model.OsuStd;
+import cc.moecraft.irc.osubot.osu.Api;
+import cc.moecraft.irc.osubot.utils.JsonUtils;
+import cc.moecraft.irc.osubot.utils.PropertiesUtil;
+import com.google.gson.JsonElement;
+import com.jfinal.kit.StrKit;
 import io.jboot.Jboot;
 import io.jboot.component.redis.JbootRedis;
+import io.jboot.utils.ArrayUtils;
 
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
 
@@ -14,6 +23,8 @@ public class CallableAndFuture {
     private static JbootRedis redis = Jboot.me().getRedis();
 
     public static void main(String[] args) {
+        final String key = PropertiesUtil.readKey("osu_key");
+        final String url = Api.User.apiUrl;
         for (int i = 0; i < 500; i++) {
             long maxId;
             if(null != redis.get("testCall")){
@@ -21,13 +32,27 @@ public class CallableAndFuture {
             }else{
                 maxId = 0;
             }
-            System.out.println("当前值是：" + maxId);
             long nextId = maxId + 1;
-            Callable<Long> callable = () -> nextId;
+            Callable<Long> callable = () -> {
+                String json = Jboot.httpPost(url + "?k=" + key + "&type=id&u=" + nextId);
+                if(!StrKit.isBlank(json)){
+                    JsonElement jsonElement = JsonUtils.parseJsonElement(json);
+                    if(jsonElement.isJsonArray()){
+                        List<Map> list = JsonUtils.getArrayByJson(json, Map.class);
+                        if(ArrayUtils.isNotEmpty(list)){
+                            String userId = list.get(0).get("user_id").toString();
+                            if(String.valueOf(nextId).equals(userId) && ArrayUtils.isNotEmpty(list)){
+                                System.out.println(userId+" 处理成功");
+                            }
+                        }
+                    }
+                }
+                return nextId;
+            };
             FutureTask<Long> future = new FutureTask<>(callable);
             new Thread(future).start();
             try {
-                System.out.println("处理后的值是：" + future.get());
+                System.out.println("当前返回值是：" + future.get());
                 //然后放进缓存
                 redis.set("testCall",String.valueOf(future.get()));
             } catch (Exception e) {
