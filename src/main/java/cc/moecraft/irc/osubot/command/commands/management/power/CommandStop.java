c9de1b1e6be434533a0a0ee1d2e9f76a2960846d
Hykilpikonna <admin@moecraft.cc>
2018-05-02 18:07:39 -0400

    [+] Stop指令添加确认

--- src/main/java/cc/moecraft/irc/osubot/command/commands/management/power/CommandStop.java
+++ src/main/java/cc/moecraft/irc/osubot/command/commands/management/power/CommandStop.java
@@ -1,11 +1,14 @@
 package cc.moecraft.irc.osubot.command.commands.management.power;
 
+import cc.moecraft.irc.osubot.Main;
 import cc.moecraft.irc.osubot.command.Command;
 import org.pircbotx.Channel;
 import org.pircbotx.User;
 import org.pircbotx.hooks.types.GenericMessageEvent;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * 此类由 Hykilpikonna 在 2018/05/02 创建!
@@ -17,6 +20,8 @@ import java.util.ArrayList;
  */
 public class CommandStop extends Command
 {
+    private static HashMap<String, Long> confirmMap = new HashMap<>(); // 玩家名对应时间
+
     public CommandStop()
     {
         super("stop", "shutdown");
@@ -25,7 +30,37 @@ public class CommandStop extends Command
     @Override
     public void run(GenericMessageEvent event, User sender, Channel channel, String command, ArrayList<String> args)
     {
-        System.exit(0);
+        AtomicBoolean shouldReturn = new AtomicBoolean(false);
+
+        new HashMap<>(confirmMap).forEach((key, value) ->
+        {
+            if ((System.currentTimeMillis() - value) >= 5000)
+            {
+                confirmMap.remove(key);
+                if (key.equals(sender.getNick()))
+                {
+                    Main.getMessenger().respond(event, "请求超过5秒, 请重新输入");
+                    shouldReturn.set(true);
+                }
+            }
+        });
+
+        if (shouldReturn.get()) return;
+
+        boolean confirmed = confirmMap.containsKey(sender.getNick());
+
+        if (confirmed)
+        {
+            Main.getMessenger().respond(event, "已执行关闭命令, 关闭成功不会发送通知");
+            System.exit(0);
+        }
+        else
+        {
+            Main.getMessenger().respond(event, "此指令将会关闭程序进程, 关闭进程后无法通过指令重新启动, 想要关闭监听器输入%prefix%disable, %prefix%enable");
+            Main.getMessenger().respond(event, "想继续关闭的话在5秒内再输入一遍%prefix%stop");
+
+            confirmMap.put(sender.getNick(), System.currentTimeMillis());
+        }
     }
 
     @Override
