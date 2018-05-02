3be76c12923de60619ae28718b30ab5944100a78
Hykilpikonna <admin@moecraft.cc>
2018-05-02 18:09:35 -0400

    [+] 禁用/启用监听的指令

---
+++ src/main/java/cc/moecraft/irc/osubot/command/commands/management/power/CommandDisable.java
@@ -0,0 +1,39 @@
+package cc.moecraft.irc.osubot.command.commands.management.power;
+
+import cc.moecraft.irc.osubot.Main;
+import cc.moecraft.irc.osubot.command.Command;
+import org.pircbotx.Channel;
+import org.pircbotx.User;
+import org.pircbotx.hooks.types.GenericMessageEvent;
+import redis.clients.jedis.Commands;
+
+import java.util.ArrayList;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/02 创建!
+ * Created by Hykilpikonna on 2018/05/02!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class CommandDisable extends Command
+{
+    public CommandDisable()
+    {
+        super("disable");
+    }
+
+    @Override
+    public void run(GenericMessageEvent event, User sender, Channel channel, String command, ArrayList<String> args)
+    {
+        Main.setEnableListening(false);
+        Main.getMessenger().respond(event, "监听已禁用!");
+    }
+
+    @Override
+    public String permissionRequired()
+    {
+        return "irc.admin.managing.disable";
+    }
+}
