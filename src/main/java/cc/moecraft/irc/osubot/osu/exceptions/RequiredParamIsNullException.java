a0929af20c5ede4747e9575bedf5223ff00ca307
Hykilpikonna <admin@moecraft.cc>
2018-05-02 20:31:55 -0400

    [+] OSUAPI获取异常抛出

---
+++ src/main/java/cc/moecraft/irc/osubot/osu/exceptions/RequiredParamIsNullException.java
@@ -0,0 +1,12 @@
+package cc.moecraft.irc.osubot.osu.exceptions;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/02 创建!
+ * Created by Hykilpikonna on 2018/05/02!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class RequiredParamIsNullException extends Exception {
+}
