eadfb0795ac0b49665896e6ed3aac959d964133f
Hykilpikonna <admin@moecraft.cc>
2018-05-04 17:41:22 -0400

    [+] 数值时间转换为字符串时间

---
+++ src/main/java/cc/moecraft/irc/osubot/utils/TimeUtils.java
@@ -0,0 +1,34 @@
+package cc.moecraft.irc.osubot.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/04 创建!
+ * Created by Hykilpikonna on 2018/05/04!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class TimeUtils
+{
+    /**
+     * 转换时间为字符串
+     *
+     * 时间格式和Date的格式一样
+     *
+     * @param format 字符串格式
+     * @param time 时间
+     * @param timeUnit 时间单位
+     * @return 字符串
+     */
+    public static String convertToString(String format, float time, TimeUnits timeUnit)
+    {
+        long millisTime = Math.round(time * TimeUnits.getTotalMultiplierBetweenTwoUnits(timeUnit, TimeUnits.Millis));
+
+        Date date = new Date(millisTime);
+        SimpleDateFormat df = new SimpleDateFormat(format);
+        return df.format(date);
+    }
+}
