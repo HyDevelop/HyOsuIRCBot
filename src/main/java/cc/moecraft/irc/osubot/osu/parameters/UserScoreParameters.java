26ded8b03c0e15b70de1217e2a8e145fcb7997da
Hykilpikonna <admin@moecraft.cc>
2018-05-05 13:58:00 -0400

    [+] 添加get_scores参数

---
+++ src/main/java/cc/moecraft/irc/osubot/osu/parameters/UserScoreParameters.java
@@ -0,0 +1,56 @@
+package cc.moecraft.irc.osubot.osu.parameters;
+
+import cc.moecraft.irc.osubot.osu.data.UserRecentData;
+import cc.moecraft.irc.osubot.osu.parameters.tags.HttpParameter;
+import lombok.*;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/05 创建!
+ * Created by Hykilpikonna on 2018/05/05!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@EqualsAndHashCode(callSuper = true)
+@Builder @AllArgsConstructor @Data @NoArgsConstructor
+public class UserScoreParameters extends ParametersBase
+{
+    // 必要的参数
+    @HttpParameter(required = true)
+    private String u;
+
+    @HttpParameter(required = true)
+    private String b;
+
+    // 不必要的参数
+    @HttpParameter(required = false)
+    private String m;
+
+    @HttpParameter(required = false)
+    private String mods;
+
+    @HttpParameter(required = false)
+    private String type;
+
+    @HttpParameter(required = false)
+    private String limit;
+
+    @Override
+    public String subURL()
+    {
+        return "scores";
+    }
+
+    @Override
+    public Class dataStorageClass()
+    {
+        return UserRecentData.class;
+    }
+
+    @Override
+    public ReturnDataType returnDataType()
+    {
+        return ReturnDataType.LIST;
+    }
+}
