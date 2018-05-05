03ae39f281141dfa87b8e61e960e15d19cb0c321
Hykilpikonna <admin@moecraft.cc>
2018-05-04 21:36:51 -0400

    [+] 添加用户recent数据类

---
+++ src/main/java/cc/moecraft/irc/osubot/osu/data/UserRecentData.java
@@ -0,0 +1,78 @@
+package cc.moecraft.irc.osubot.osu.data;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import io.shardingjdbc.core.routing.type.all.DatabaseAllRoutingEngine;
+import lombok.*;
+
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/04 创建!
+ * Created by Hykilpikonna on 2018/05/04!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRecentData extends DataBase
+{
+    @SerializedName("beatmap_id")
+    @Expose
+    public int beatmapId;
+
+    @SerializedName("score")
+    @Expose
+    public long score;
+
+    @SerializedName("maxcombo")
+    @Expose
+    public int maxCombo;
+
+    @SerializedName("count50")
+    @Expose
+    public int count50;
+
+    @SerializedName("count100")
+    @Expose
+    public int count100;
+
+    @SerializedName("count300")
+    @Expose
+    public int count300;
+
+    @SerializedName("countmiss")
+    @Expose
+    public int countMiss;
+
+    @SerializedName("countkatu")
+    @Expose
+    public int count100p; // 喝
+
+    @SerializedName("countgeki")
+    @Expose
+    public int count300p; // 激
+
+    @SerializedName("perfect")
+    @Expose
+    public int perfect;
+
+    @SerializedName("enabled_mods")
+    @Expose
+    public int enabledMods;
+
+    @SerializedName("user_id")
+    @Expose
+    public int userId;
+
+    @SerializedName("date")
+    @Expose
+    public String date;
+
+    @SerializedName("rank")
+    @Expose
+    public String rank;
+}
