70c7521b713e614f2aaa2d3815f3154cf004807f
Hykilpikonna <admin@moecraft.cc>
2018-05-06 19:24:57 -0400

    [+] 添加异常

---
+++ src/main/java/cc/moecraft/irc/osubot/osu/exceptions/BeatmapScoreNotEnoughException.java
@@ -0,0 +1,12 @@
+package cc.moecraft.irc.osubot.osu.exceptions;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/06 创建!
+ * Created by Hykilpikonna on 2018/05/06!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class BeatmapScoreNotEnoughException extends Exception {
+}
