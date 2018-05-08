60829c6a64269c16097c0faf3c8c3c7aec222ec6
Hykilpikonna <admin@moecraft.cc>
2018-05-08 19:29:27 -0400

    [+] 没有找到Json异常

---
+++ src/main/java/cc/moecraft/irc/osubot/osu/exceptions/JsonNotFoundInHtmlException.java
@@ -0,0 +1,11 @@
+package cc.moecraft.irc.osubot.osu.exceptions;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/08 创建!
+ * Created by Hykilpikonna on 2018/05/08!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class JsonNotFoundInHtmlException extends Throwable {}
