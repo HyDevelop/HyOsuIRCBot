59b171451acfcd531c12b8363c2b461bad122e03
Hykilpikonna <admin@moecraft.cc>
2018-05-07 20:31:58 -0400

    [+] PP计算数据测试

---
+++ src/test/java/cc/moecraft/test/PpCalculatorTest.java
@@ -0,0 +1,24 @@
+package cc.moecraft.test;
+
+import cc.moecraft.irc.osubot.osu.calculator.PpCalculator;
+import cc.moecraft.logger.DebugLogger;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/07 创建!
+ * Created by Hykilpikonna on 2018/05/07!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class PpCalculatorTest
+{
+    private static DebugLogger logger = new DebugLogger("PpCalculatorTest", true);
+
+    public static void main(String[] args)
+    {
+        logger.debug("Mania测试 (*1.9, OD 6, Score 617225, ACC 92.69, Objects 326): ");
+
+        logger.debug("- " + PpCalculator.calculateManiaNoModPP(1.9, 6, 617225, 92.69, 326) + " PP");
+    }
+}
