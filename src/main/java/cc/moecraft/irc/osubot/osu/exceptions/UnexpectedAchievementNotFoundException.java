9a6444abf641422fe1e0b0b05f5551838a62fd7d
Hykilpikonna <admin@moecraft.cc>
2018-05-10 19:19:57 -0400

    [+] 成就未找到异常

---
+++ src/main/java/cc/moecraft/irc/osubot/osu/exceptions/UnexpectedAchievementNotFoundException.java
@@ -0,0 +1,22 @@
+package cc.moecraft.irc.osubot.osu.exceptions;
+
+import cc.moecraft.irc.osubot.osu.data.AchievementData;
+import com.google.gson.Gson;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/10 创建!
+ * Created by Hykilpikonna on 2018/05/10!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class UnexpectedAchievementNotFoundException extends UnexpectedException
+{
+    public UnexpectedAchievementNotFoundException(AchievementData achievementData)
+    {
+        super("错误, 成就" + achievementData.getId() + "未找到");
+
+        additionalObjects.put("Achievement", new Gson().toJson(achievementData));
+    }
+}
