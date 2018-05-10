ad396268571d8fa1b1eea493c593bef874ad0659
Hykilpikonna <admin@moecraft.cc>
2018-05-10 19:00:24 -0400

    [U] 更新成就格式

--- src/main/java/cc/moecraft/irc/osubot/achievement/achievements/AchievementOverconfident.java
+++ src/main/java/cc/moecraft/irc/osubot/achievement/achievements/AchievementOverconfident.java
@@ -3,6 +3,8 @@ package cc.moecraft.irc.osubot.achievement.achievements;
 import cc.moecraft.irc.osubot.achievement.Achievement;
 import cc.moecraft.irc.osubot.osu.Mod;
 import cc.moecraft.irc.osubot.osu.Mods;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * 此类由 Hykilpikonna 在 2018/05/08 创建!
@@ -15,37 +17,69 @@ import cc.moecraft.irc.osubot.osu.Mods;
 public class AchievementOverconfident extends Achievement
 {
     @Override
-    public String officialAchievementName()
+    public long getId()
+    {
+        return 0;
+    }
+
+    @Override
+    public String getName()
     {
         return "Overconfident";
     }
 
     @Override
-    public String tutorial()
+    public String getGrouping()
+    {
+        return null;
+    }
+
+    @Override
+    public long getOrdering() {
+        return 0;
+    }
+
+    @Override
+    public String getSlug() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public int getMode() {
+        return 0;
+    }
+
+    @Override
+    public String getTutorial()
     {
         return "任意谱面开DT+HR打出低于40%的准确率";
     }
 
     @Override
-    public long recommendedMap()
+    public long getRecommendedMap()
     {
         return 434841;
     }
 
     @Override
-    public Mods mods()
+    public Mods getMods()
     {
         return new Mods(Mod.DoubleTime, Mod.HardRock);
     }
 
     @Override
-    public double completionTimeInMinutes()
+    public double getCompletionTimeInMinutes()
     {
         return 1;
     }
 
     @Override
-    public int averageRetryCount()
+    public int getAverageRetryCount()
     {
         return 1;
     }
