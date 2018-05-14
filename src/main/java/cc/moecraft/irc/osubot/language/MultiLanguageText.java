f9bb75fd2cfe2899118abfc5f2b92174c728312d
Hykilpikonna <admin@moecraft.cc>
2018-05-14 17:36:41 -0400

    [+] 多语言对象

---
+++ src/main/java/cc/moecraft/irc/osubot/language/MultiLanguageText.java
@@ -0,0 +1,30 @@
+package cc.moecraft.irc.osubot.language;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/14 创建!
+ * Created by Hykilpikonna on 2018/05/14!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+@Data @RequiredArgsConstructor @AllArgsConstructor
+public class MultiLanguageText
+{
+    @NotNull
+    private final String text;
+    private Type type = Type.DIRECT_TEXT;
+
+    public enum Type
+    {
+        LANGUAGE_NODE,  // 语言节点形式
+        DIRECT_TEXT,    // 消息形式
+        EMPTY           // 空的
+    }
+}
