686985b9cca17e65efaa4e80c74c685cee943a53
蠢狼 <1091569752@qq.com>
2018-05-16 20:07:44 +0800

    DAO工厂补上新的service

--- src/main/java/cc/moecraft/irc/osubot/database/factory/DAOFactory.java
+++ src/main/java/cc/moecraft/irc/osubot/database/factory/DAOFactory.java
@@ -1,11 +1,17 @@
 package cc.moecraft.irc.osubot.database.factory;
 
+import cc.moecraft.irc.osubot.database.service.OsuLuaService;
 import cc.moecraft.irc.osubot.database.service.OsuService;
+import cc.moecraft.irc.osubot.database.service.impl.OsuLuaServiceImpl;
 import cc.moecraft.irc.osubot.database.service.impl.OsuServiceImpl;
 
 public class DAOFactory {
 
     public static OsuService getOsuService(){
         return new OsuServiceImpl();
+    }
+
+    public static OsuLuaService getOsuLuaService(){
+        return new OsuLuaServiceImpl();
     }
 }
