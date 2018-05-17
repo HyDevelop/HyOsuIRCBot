f425acc87a6c30e009c08177ac464d794bb129f7
Hykilpikonna <admin@moecraft.cc>
2018-05-17 18:48:34 -0400

    [U] 更新!update指令语言文件格式

--- src/main/java/cc/moecraft/irc/osubot/command/commands/osu/CommandUpdate.java
+++ src/main/java/cc/moecraft/irc/osubot/command/commands/osu/CommandUpdate.java
@@ -83,10 +83,10 @@ public class CommandUpdate extends Command
             return MultiLanguageText.directText(format);
         } catch (IllegalAccessException | RequiredParamIsNullException | MalformedURLException e) {
             e.printStackTrace();
-            return MultiLanguageText.languageNode("error_unknown_backend_error");
+            return MultiLanguageText.languageNode("errors.unknown_backend_error");
         } catch (JsonEmptyException e) {
             e.printStackTrace();
-            return MultiLanguageText.languageNode("error_unknown_username_2");
+            return MultiLanguageText.languageNode("errors.unknown_username");
             // TODO: 报错收集系统
         }
     }
