454ef397fdcc8e570ddd3969e28e987a61d453a5
Hykilpikonna <admin@moecraft.cc>
2018-05-22 13:39:07 -0400

    [+] 机器人移动数据类

---
+++ src/main/java/cc/moecraft/irc/osubot/command/commands/fun/minigames/fingers/FingersBotMovedData.java
@@ -0,0 +1,19 @@
+package cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers;
+
+import lombok.Data;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/22 创建!
+ * Created by Hykilpikonna on 2018/05/22!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+@Data
+public class FingersBotMovedData
+{
+    public FingersData data;
+    public int fromBotFinger;
+    public int toPlayerFinger;
+}
