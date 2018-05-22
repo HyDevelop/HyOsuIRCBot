e309c2beebcde3aa542b03f8fce89d67d24c92b7
Hykilpikonna <admin@moecraft.cc>
2018-05-22 12:53:23 -0400

    [+] 判定玩家输入数据是否合理

--- src/main/java/cc/moecraft/irc/osubot/command/commands/fun/minigames/fingers/FingersAI.java
+++ src/main/java/cc/moecraft/irc/osubot/command/commands/fun/minigames/fingers/FingersAI.java
@@ -1,6 +1,7 @@
 package cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers;
 
 import cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.exceptions.DataSetInvalidException;
+import cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.exceptions.PlayerInputInvalidException;
 
 /**
  * 此类由 Hykilpikonna 在 2018/05/22 创建!
@@ -12,9 +13,11 @@ import cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.exceptions.
  */
 public class FingersAI
 {
-    public static FingersData calculateNext(FingersData current, int fromPlayerFinger, int toBotFinger) throws DataSetInvalidException
+    public static FingersData calculateNext(FingersData current, int fromPlayerFinger, int toBotFinger) throws DataSetInvalidException, PlayerInputInvalidException
     {
         if (!current.isValid()) throw new DataSetInvalidException();
+
+        if (!(FingersData.isValidNumber(fromPlayerFinger) && FingersData.isValidNumber(toBotFinger))) throw new PlayerInputInvalidException();
 
         return current; // TODO: 返回数据
     }
