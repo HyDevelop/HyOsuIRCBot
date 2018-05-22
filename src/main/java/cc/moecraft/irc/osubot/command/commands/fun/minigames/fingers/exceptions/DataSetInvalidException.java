45fad4c8b2e7348e462f2143ace5f98f0f03b55e
Hykilpikonna <admin@moecraft.cc>
2018-05-22 12:49:49 -0400

    [+] 数据组不合理异常

---
+++ src/main/java/cc/moecraft/irc/osubot/command/commands/fun/minigames/fingers/exceptions/DataSetInvalidException.java
@@ -0,0 +1,14 @@
+package cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.exceptions;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/22 创建!
+ * Created by Hykilpikonna on 2018/05/22!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class DataSetInvalidException extends Exception
+{
+
+}
