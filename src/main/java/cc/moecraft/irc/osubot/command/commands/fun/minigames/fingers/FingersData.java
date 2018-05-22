1e69f0a79ac2863348ae1f766b3120b0f34bc6b0
Hykilpikonna <admin@moecraft.cc>
2018-05-22 12:44:13 -0400

    [+] 碰数游戏数据类

---
+++ src/main/java/cc/moecraft/irc/osubot/command/commands/fun/minigames/fingers/FingersData.java
@@ -0,0 +1,21 @@
+package cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers;
+
+import lombok.Data;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/22 创建!
+ * Created by Hykilpikonna on 2018/05/22!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@Data
+public class FingersData
+{
+    public int playerHand1;
+    public int playerHand2;
+
+    public int botHand1;
+    public int botHand2;
+}
