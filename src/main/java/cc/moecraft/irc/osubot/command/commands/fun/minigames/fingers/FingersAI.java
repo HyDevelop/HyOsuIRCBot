7707e63089db08fe25071b7cfa5c1d07500ce4d6
Hykilpikonna <admin@moecraft.cc>
2018-05-22 13:00:30 -0400

    [+] 判断玩家输入是否在手上

--- src/main/java/cc/moecraft/irc/osubot/command/commands/fun/minigames/fingers/FingersAI.java
+++ src/main/java/cc/moecraft/irc/osubot/command/commands/fun/minigames/fingers/FingersAI.java
@@ -1,6 +1,7 @@
 package cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers;
 
 import cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.exceptions.DataSetInvalidException;
+import cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.exceptions.InputNumberNotFoundException;
 import cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.exceptions.PlayerInputInvalidException;
 
 /**
@@ -13,11 +14,14 @@ import cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.exceptions.
  */
 public class FingersAI
 {
-    public static FingersData calculateNext(FingersData current, int fromPlayerFinger, int toBotFinger) throws DataSetInvalidException, PlayerInputInvalidException
+    public static FingersData calculateNext(FingersData current, int fromPlayerFinger, int toBotFinger) throws DataSetInvalidException, PlayerInputInvalidException, InputNumberNotFoundException
     {
         if (!current.isValid()) throw new DataSetInvalidException();
 
         if (!(FingersData.isValidNumber(fromPlayerFinger) && FingersData.isValidNumber(toBotFinger))) throw new PlayerInputInvalidException();
+
+        if (current.playerHand1 != fromPlayerFinger && current.playerHand2 != fromPlayerFinger) throw new InputNumberNotFoundException(InputNumberNotFoundException.Type.PLAYER);
+        if (current.botHand1 != toBotFinger && current.botHand2 != toBotFinger) throw new InputNumberNotFoundException(InputNumberNotFoundException.Type.BOT);
 
         return current; // TODO: 返回数据
     }
