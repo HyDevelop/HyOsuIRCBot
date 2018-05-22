917bc7b681663572114128de8de8c1f5fe78e981
Hykilpikonna <admin@moecraft.cc>
2018-05-22 13:08:38 -0400

    [+] 判断游戏是否已经结束

--- src/main/java/cc/moecraft/irc/osubot/command/commands/fun/minigames/fingers/FingersAI.java
+++ src/main/java/cc/moecraft/irc/osubot/command/commands/fun/minigames/fingers/FingersAI.java
@@ -1,6 +1,7 @@
 package cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers;
 
 import cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.exceptions.DataSetInvalidException;
+import cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.exceptions.GameEndedException;
 import cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.exceptions.InputNumberNotFoundException;
 import cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.exceptions.PlayerInputInvalidException;
 
@@ -14,12 +15,18 @@ import cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.exceptions.
  */
 public class FingersAI
 {
-    public static FingersData calculateNext(FingersData current, int fromPlayerFinger, int toBotFinger) throws DataSetInvalidException, PlayerInputInvalidException, InputNumberNotFoundException
+    public static FingersData calculateNext(FingersData current, int fromPlayerFinger, int toBotFinger)
+            throws DataSetInvalidException, PlayerInputInvalidException, InputNumberNotFoundException, GameEndedException
     {
+        // 判断数字是否为1到9
         if (!current.isValid()) throw new DataSetInvalidException();
-
         if (!(FingersData.isValidNumber(fromPlayerFinger) && FingersData.isValidNumber(toBotFinger))) throw new PlayerInputInvalidException();
 
+        // 判断游戏是否已经结束
+        if (current.playerHand1 == 0 && current.playerHand2 == 0) throw new GameEndedException(GameEndedException.WinnerType.PLAYER);
+        if (current.botHand1 == 0 && current.botHand2 == 0) throw new GameEndedException(GameEndedException.WinnerType.BOT);
+
+        // 判断输入内容是否在玩家或者机器人手中
         if (current.playerHand1 != fromPlayerFinger && current.playerHand2 != fromPlayerFinger) throw new InputNumberNotFoundException(InputNumberNotFoundException.Type.PLAYER);
         if (current.botHand1 != toBotFinger && current.botHand2 != toBotFinger) throw new InputNumberNotFoundException(InputNumberNotFoundException.Type.BOT);
 
