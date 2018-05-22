6b076d28a00875e105c85cae94952f7f633a5247
Hykilpikonna <admin@moecraft.cc>
2018-05-22 12:59:51 -0400

    [+] 输入的数不存在异常

---
+++ src/main/java/cc/moecraft/irc/osubot/command/commands/fun/minigames/fingers/exceptions/InputNumberNotFoundException.java
@@ -0,0 +1,26 @@
+package cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/22 创建!
+ * Created by Hykilpikonna on 2018/05/22!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+public class InputNumberNotFoundException extends Exception
+{
+    private Type type;
+
+    public enum Type
+    {
+        PLAYER, BOT
+    }
+}
