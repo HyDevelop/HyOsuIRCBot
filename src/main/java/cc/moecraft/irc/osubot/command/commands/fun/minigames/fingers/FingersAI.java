3a900c4ca2bc1e9bbde93b91fcd48ce92c3fa799
Hykilpikonna <admin@moecraft.cc>
2018-05-22 14:53:54 -0400

    [+] 应用更改

--- src/main/java/cc/moecraft/irc/osubot/command/commands/fun/minigames/fingers/FingersAI.java
+++ src/main/java/cc/moecraft/irc/osubot/command/commands/fun/minigames/fingers/FingersAI.java
@@ -5,6 +5,8 @@ import cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.exceptions.
 import cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.exceptions.InputNumberNotFoundException;
 import cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.exceptions.PlayerInputInvalidException;
 
+import java.util.Arrays;
+
 /**
  * 此类由 Hykilpikonna 在 2018/05/22 创建!
  * Created by Hykilpikonna on 2018/05/22!
@@ -19,7 +21,7 @@ public class FingersAI
      * 通过当前局面和获取到的玩家移动来计算玩家移动后的局面
      * @return 玩家移动后的局面
      */
-    public static FingersData calculateNext(FingersData current, int fromPlayerFinger, int toBotFinger)
+    public static FingersData applyPlayerMove(FingersData current, int fromPlayerFinger, int toBotFinger)
             throws DataSetInvalidException, PlayerInputInvalidException, InputNumberNotFoundException, GameEndedException
     {
         // 判断数字是否为1到9
@@ -27,8 +29,8 @@ public class FingersAI
         if (!(FingersData.isValidNumber(fromPlayerFinger) && FingersData.isValidNumber(toBotFinger))) throw new PlayerInputInvalidException();
 
         // 判断游戏是否已经结束
-        if (current.getPlayerHand()[1] == 0 && current.getPlayerHand()[2] == 0) throw new GameEndedException(GameEndedException.WinnerType.PLAYER);
-        if (current.getBotHand()   [1] == 0 && current.getBotHand()   [2] == 0) throw new GameEndedException(GameEndedException.WinnerType.BOT);
+        if (current.getPlayerHand()[0] == 0 && current.getPlayerHand()[1] == 0) throw new GameEndedException(GameEndedException.WinnerType.PLAYER);
+        if (current.getBotHand()   [0] == 0 && current.getBotHand()   [1] == 0) throw new GameEndedException(GameEndedException.WinnerType.BOT);
 
         // 获取玩家移动后的局面
         int playerHandIndex = current.findPlayerIndex(fromPlayerFinger);
