839a1479a40b36e358687e91bfa7368727a6de63
Hykilpikonna <admin@moecraft.cc>
2018-05-23 17:03:57 -0400

    [+] 一步类

---
+++ src/main/java/cc/moecraft/irc/osubot/command/commands/fun/minigames/fingers/MLFingersMove.java
@@ -0,0 +1,23 @@
+package cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers;
+
+import cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.exceptions.InputNumberNotFoundException;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/23 创建!
+ * Created by Hykilpikonna on 2018/05/23!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+@Data @AllArgsConstructor
+public class MLFingersMove
+{
+    public FingersPlayerType playerType;
+    public FingersSituation lastSituation; // 移动之前的局面
+    public FingersSituation currentSituation; // 移动之后的局面
+    public int moveFrom;
+    public int moveTo;
+}
