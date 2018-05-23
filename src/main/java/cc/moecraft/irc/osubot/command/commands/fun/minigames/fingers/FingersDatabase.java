9b0f72f34fcdee57180a23fc1741e9eab419ad27
Hykilpikonna <admin@moecraft.cc>
2018-05-22 20:28:33 -0400

    [+] 存最佳移动的数据库

---
+++ src/main/java/cc/moecraft/irc/osubot/command/commands/fun/minigames/fingers/FingersDatabase.java
@@ -0,0 +1,33 @@
+package cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers;
+
+import cc.moecraft.irc.osubot.Main;
+import cc.moecraft.yaml.Config;
+import io.swagger.models.auth.In;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/22 创建!
+ * Created by Hykilpikonna on 2018/05/22!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class FingersDatabase extends Config
+{
+    public FingersDatabase()
+    {
+        super(Main.PATH, "FingersDatabase", "yml", false, true);
+    }
+
+    @Override
+    public void readConfig()
+    {
+
+    }
+
+    @Override
+    public void writeDefaultConfig()
+    {
+
+    }
+}
