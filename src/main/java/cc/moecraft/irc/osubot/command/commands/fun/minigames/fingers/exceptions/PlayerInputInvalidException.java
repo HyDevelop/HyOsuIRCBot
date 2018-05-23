53941da7359283af6a92703b66fbb619929a0083
Hykilpikonna <admin@moecraft.cc>
2018-05-23 16:59:01 -0400

    [+] 玩家输入错误异常

---
+++ src/main/java/cc/moecraft/irc/osubot/command/commands/fun/minigames/fingers/exceptions/PlayerInputInvalidException.java
@@ -0,0 +1,17 @@
+package cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/22 创建!
+ * Created by Hykilpikonna on 2018/05/22!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class PlayerInputInvalidException extends Exception
+{
+}
