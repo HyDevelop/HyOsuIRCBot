54e535636455b26319b57b226df787863124c51d
Hykilpikonna <admin@moecraft.cc>
2018-05-23 17:07:15 -0400

    [+] 碰数游戏添加移动一步方法

--- src/main/java/cc/moecraft/irc/osubot/command/commands/fun/minigames/fingers/MLFingersGame.java
+++ src/main/java/cc/moecraft/irc/osubot/command/commands/fun/minigames/fingers/MLFingersGame.java
@@ -26,4 +26,29 @@ public class MLFingersGame
     public MLFingersMove lastMove;
     public boolean ended = false;
     public MLFingersDatabase database;
+
+    /**
+     * 移动一步
+     * @param moveFrom 移动者手上的数
+     * @param moveTo 对面手上的数
+     * @param player 移动者是玩家还是机器人
+     * @return 这步
+     * @throws InputNumberNotFoundException 输入错误
+     * @throws NotYourTurnException 不该你走
+     */
+    public MLFingersMove move(int moveFrom, int moveTo, FingersPlayerType player) throws InputNumberNotFoundException, NotYourTurnException, PlayerInputInvalidException, GameEndedException
+    {
+        if (lastMove.playerType == player) throw new NotYourTurnException(player, player.getTheOther());
+        if (!FingersSituation.isValidNumber(moveFrom) || moveFrom == 0) throw new PlayerInputInvalidException(moveFrom);
+        if (!FingersSituation.isValidNumber(moveTo) || moveTo == 0) throw new PlayerInputInvalidException(moveTo);
+
+        MLFingersMove move = MLFingersMove.nextMove(player, lastMove.getCurrentSituation(), moveFrom, moveTo);
+
+        moves.add(move);
+        lastMove = move;
+
+        if (lastMove.currentSituation.getPlayerHand()[0] == 0 && lastMove.currentSituation.getPlayerHand()[1] == 0) throw new GameEndedException(Player, this);
+        if (lastMove.currentSituation.getBotHand()   [0] == 0 && lastMove.currentSituation.getBotHand()   [1] == 0) throw new GameEndedException(Bot, this);
+        return move;
+    }
 }
