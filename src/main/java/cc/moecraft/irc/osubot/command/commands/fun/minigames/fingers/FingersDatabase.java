2f0d3c4639e31fa200852a141447e9e16d7c8391
Hykilpikonna <admin@moecraft.cc>
2018-05-22 20:28:56 -0400

    [+] 添加最佳移动数据方法

--- src/main/java/cc/moecraft/irc/osubot/command/commands/fun/minigames/fingers/FingersDatabase.java
+++ src/main/java/cc/moecraft/irc/osubot/command/commands/fun/minigames/fingers/FingersDatabase.java
@@ -19,6 +19,25 @@ public class FingersDatabase extends Config
         super(Main.PATH, "FingersDatabase", "yml", false, true);
     }
 
+    /**
+     * 添加/设置一条最佳移动数据
+     *
+     * 记录格式例子:
+     *   如果当前局面为 [3, 2] [8, 1]
+     *   最佳移动为 [2, 8]
+     *   胜利数为54, 失败数为27, 循环数为17, 那么会记录成这样:
+     *
+     *   D3281: 2/8/54/27/17
+     *
+     * @param move 最佳移动数据
+     */
+    public void setBestMoveData(FingersWinRatioData move)
+    {
+        set("D" + move.getBotMoveData().getData().toString(), String.format("%s/%s/%s/%s/%s",
+                        move.getBotMoveData().getFromBotFinger(),
+                        move.getBotMoveData().getToPlayerFinger(),
+                        move.getWin(), move.getLose(), move.getDraw()));
+    }
     @Override
     public void readConfig()
     {
