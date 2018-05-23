757960d778bb81765760ff66282f1580924c0e91
Hykilpikonna <admin@moecraft.cc>
2018-05-23 17:00:44 -0400

    [+] 机器学习碰数游戏数据库

---
+++ src/main/java/cc/moecraft/irc/osubot/command/commands/fun/minigames/fingers/MLFingersDatabase.java
@@ -0,0 +1,34 @@
+package cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers;
+
+import cc.moecraft.irc.osubot.Main;
+import cc.moecraft.yaml.Config;
+
+import java.io.File;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/23 创建!
+ * Created by Hykilpikonna on 2018/05/23!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class MLFingersDatabase extends Config
+{
+    public MLFingersDatabase()
+    {
+        super(Main.PATH + File.separator + "experimental", "MLFingersDatabase", "yml", false, true);
+    }
+
+    @Override
+    public void readConfig()
+    {
+
+    }
+
+    @Override
+    public void writeDefaultConfig()
+    {
+
+    }
+}
