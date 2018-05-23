f73978d02f007f0b07e130d04e2ec3f88e031fc4
Hykilpikonna <admin@moecraft.cc>
2018-05-23 16:58:19 -0400

    [+] 不该你走异常

---
+++ src/main/java/cc/moecraft/irc/osubot/command/commands/fun/minigames/fingers/exceptions/NotYourTurnException.java
@@ -0,0 +1,20 @@
+package cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.exceptions;
+
+import cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.FingersPlayerType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/23 创建!
+ * Created by Hykilpikonna on 2018/05/23!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data @AllArgsConstructor
+public class NotYourTurnException extends Exception
+{
+}
