d3c56caf213264264592a059980d164ea029a642
Hykilpikonna <admin@moecraft.cc>
2018-05-23 16:55:55 -0400

    [+] 游戏结束异常

---
+++ src/main/java/cc/moecraft/irc/osubot/command/commands/fun/minigames/fingers/exceptions/GameEndedException.java
@@ -0,0 +1,21 @@
+package cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.exceptions;
+
+import cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.FingersPlayerType;
+import cc.moecraft.irc.osubot.command.commands.fun.minigames.fingers.MLFingersGame;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/22 创建!
+ * Created by Hykilpikonna on 2018/05/22!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class GameEndedException extends Exception
+{
+}
